6ea2a4443b5916aca933137637881cb12aa3ce98
aidendorian <resorttoaiden@gmail.com>
2025-12-05 06:07:51 +0000

    init

--- src/dataloaders.py
+++ src/dataloaders.py
@@ -1,7 +1,24 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+from pathlib import Path
+from PIL import Image
 from torchvision.transforms import Compose
-from torchvision import transforms, datasets
+from torchvision import transforms
 
+class ImageDataset(Dataset):
+    def __init__(self, root, transform=None):
+        self.root = Path(root)
+        self.img_pth = list(self.root.rglob("*.jpg"))
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.img_pth)
+    
+    def __getitem__(self, index):
+        img = Image.open(self.img_pth[index].convert("RGB"))
+        if self.transform:
+            img = self.transform(img)
+        return img
+               
 transform = Compose([
     transforms.CenterCrop(256),
     transforms.ToTensor(),
@@ -13,10 +30,10 @@ def get_dataloaders(batch_size:int=16,
                    prefetch_factor:int=2,
                    persistent_workers:int=2):
     
-    content_dataset = datasets.ImageFolder("../data/content",
-                                           transform=transform)
-    style_dataset = datasets.ImageFolder("../data/style",
-                                         transform=transform)
+    content_dataset = ImageDataset(root="data/content",
+                                   transform=transform)
+    style_dataset = ImageDataset(root="data/style",
+                                 transform=transform)
     
     content_loader = DataLoader(dataset=content_dataset,
                                 batch_size=batch_size,
--- src/image_nst.py
+++ src/image_nst.py
@@ -1,2 +1,5 @@
 import torch
 
+STYLE_LAYERS = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1']
+CONTENT_LAYERS = ['conv4_2']
+
