b813f2312e4c755fdca31a18dc1e6d6b2c951996
aidendorian <resorttoaiden@gmail.com>
2025-12-07 12:09:43 +0000

    - Changed Loss from L2 to L1 - Removed the forward method from FeatureExtractor - Minor Changes

--- src/utils.py
+++ src/utils.py
@@ -1,66 +1,62 @@
-from torch.nn import MSELoss, Module
-from torch import bmm
-from torchvision.models import vgg19
+from torch.nn import Module
+from torch import mm
+from torchvision.models import vgg19, VGG19_Weights
+from torch.nn.functional import l1_loss
 
-def content_loss(content_fm, generated_content_fm):
-    """Calculates Content Loss
-
-    Args:
-        content : Feature Map of the Content Image from the conv4_2 layer of VGG19
-        generated_content : Feature Map of the Generated Image from the conv4_2 layer of VGG19
-    
-    Returns:
-        Content Loss
-    """
-    loss_fn = MSELoss()
-    content_loss = loss_fn(generated_content_fm, content_fm)
-    return content_loss
-
-def gram_matrix(feature_map):
-    """Generates Gram matrix of the feature Map
+class loss_content(Module):
+    """Calculate the Content Loss
 
     Args:
-        feature_map : Feature Map
-
-    Returns:
-        Gram Matrix
-    """
-    b, c, h, w = feature_map.shape
-    features = feature_map.view(b, c, h*w)
-    gram = bmm(features, features.transpose(1, 2))/(b*c*h*w)
-    return gram
-
-class StyleLoss(Module):
-    """Calculate the Style Loss
-
-    Args:
-        target_features : Style Image Feature Map of ith Layer of VGG19
+        target_features : Content Image Feature Map
     """
     def __init__(self, target_features):
         super().__init__()
-        self.target_features = gram_matrix(target_features).detach()
+        self.target = target_features.detach()
+        self.content_loss = None
+        
+    def forward(self, x):
+        self.content_loss = l1_loss(x, self.target)
+        return x
+
+def gram_matrix(feature_map):
+    """Generates Gram matrix of the feature Map
+    Args:
+        feature_map : Feature Map
+    Returns:
+        Gram Matrix
+    """
+    c, h, w = feature_map.shape
+    features = feature_map.view(c, h*w)
+    gram = mm(features, features.t())
+    return gram.div(c*h*w)
+
+class loss_style(Module):
+    """Calculate the Style Loss
+    Args:
+        target_features : Style Image Feature Map from ith Layer of VGG19
+    """
+    def __init__(self, target_features):
+        super().__init__()
+        self.target_features = gram_matrix(target_features.detach()).detach()
         self.style_loss = 0.
         
     def forward(self, x):
         G = gram_matrix(x)
-        loss_fn = MSELoss()
-        self.style_loss = loss_fn(G, self.target_features)
+        self.style_loss = l1_loss(G, self.target_features)
         return x
     
 class FeatureExtractor(Module):
     """Extracts Feature Maps from Specific Layers of VGG19
-
     Args:
         style_layers : List of Indexes of layers from which style features need to be extracted from.
         content_layer : Index of the layer from which the content feature need to extracted from.
     """
-    def __init__(self, style_layers, content_layer):
+    def __init__(self, content_layer, style_layers, device='cuda'):
         super().__init__()
-        extractor_model = vgg19(weights='DEFAULT')
-        extractor_features = extractor_model.features
+        extractor_features = vgg19(weights=VGG19_Weights.DEFAULT).to(device).features.eval()
         
         for params in extractor_features.parameters():
-            params.requires_grad(False)
+            params.requires_grad = False
             
         self.style_layers = style_layers
         self.content_layer = content_layer
@@ -68,59 +64,33 @@ class FeatureExtractor(Module):
         
     def style_extractor(self, x):
         """Extracts Feature Maps from the specific VGG19 layers
-
         Args:
             x : Image Tensor from which Style Features need to be extacted. 
-
         Returns:
-            features : Dictionary of all the feature maps extracted.
+            features : List of all the feature maps extracted.
         """
         features = []
         
         for i, layer in enumerate(self.vgg19):
             x = layer(x)
-            
             if i in self.style_layers:
                 features.append(x)
-                
             if i == max(self.style_layers):
                 break
-                
         return features
         
     def content_extractor(self, x):
         """Extracts Feature Maps from the specific VGG19 layers
-
         Args:
             x : Image Tensor from which Content Features need to be extacted. 
-
         Returns:
-            features : Dictionary of all the feature maps extracted.
+            features : Content feature map extracted.
         """
         features = 0.
         
         for i, layer in enumerate(self.vgg19):
             x = layer(x)
-            
             if i == self.content_layer:
                 features = x
                 break
-                
-        return features
-        
-    def forward(self, x):
-        features = []
-        
-        for i, layer in enumerate(self.vgg19):
-            x = layer(x)
-            
-            if i in self.style_layers:
-                features.append(x)
-                
-            if i == self.content_layer:
-                features.append(x)
-                
-            if i == max(self.style_layers):
-                break
-            
         return features
